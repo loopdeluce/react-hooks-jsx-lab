c52ef1969e908ba5893a0a1239ba749e133a3a51
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-07 10:47:39 -0600

    commit tree 44949a0be63fc2585d6e233407bd025c809c5504

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+// a <div> element with an id of "home"
+// inside the div element, there should be one child element:
+// an <h1> element, with the style attribute set to a color of "firebrick", and the content of "Your Name is a Web Developer from Your City"
+// use JSX expression syntax to use the imported name and city variables inside your JSX elements
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const h1Style = {
+    color: 'firebrick'
+  };
+  const text = `${name} is a Web Developer from ${city}`
+
+  return (
+  <div id='home'>
+    <h1 style={h1Style}>{text}</h1>
+  </div>
+  );
 }
 
 export default Home;
